66441f9936374b4142e3a33a6e35eed65245de87
Akhilesh2709 <bemalkhedakhilesh@outlook.com>
2022-07-29 15:24:20 +0000

    updated teestbench

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,27 +2,22 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
+from random import randint
+
+
 
 @cocotb.test()
 async def test_mux(dut):
-    """Test for mux2"""
-    sel=1
-    dut.sel.value=sel
-    
+    """Test for inp12"""
+    dut.sel.value=12
+    dut.inp12.value=random.randint(0,3)
     await Timer (2,units='ns')
-    assert dut.x.value== mux(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, 
-           inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17,
-           inp18, inp19, inp20, inp21, inp22, inp23, inp24, inp25, inp26,
-           inp27, inp28, inp29, inp30),f'mux is incorrect: {dut.x.value}!=inp1'
+    assert dut.out.value== dut.inp12.value,f'mux output is incorrect:{dut.inp12.value}!={dut.out.value}'
 
-@cocotb.test()
-async def randomised_test_mux(dut):
-    'test for two random multiple numbers'
-    for i in range (31):
-        sel=random.randint(0,31)
-        dut.sel.value=sel
-        await Timer(2,units='ns')
-        assert dut.x.value == mux(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, 
-           inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17,
-           inp18, inp19, inp20, inp21, inp22, inp23, inp24, inp25, inp26,
-           inp27, inp28, inp29, inp30),'randomised test failed with:{sel}={x}'.format (sel=dut.sel.value,x=dut.x.value)  
+async def test_mux(dut):
+    'Test for inp30'
+    dut.sel.value=30
+    dut.inp30.value=random.randint(0,3)
+    await Timer (2,units='ns')
+    assert dut.out.value==dut.inp30.value,f'mux output is incorrect:{dut.inp30.value}!={dut.out.value}'
