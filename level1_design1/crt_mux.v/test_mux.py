1cb614f7087dc8805ee4ad293ea279d8c8503223
Akhilesh2709 <bemalkhedakhilesh@outlook.com>
2022-07-30 14:38:33 +0000

    updated testbench

---
+++ level1_design1/crt_mux.v/test_mux.py
@@ -0,0 +1,22 @@
+import cocotb
+from cocotb.triggers import Timer
+import random
+from random import randint
+
+
+
+@cocotb.test()
+async def test_mux(dut):
+    """Test for inp12"""
+    dut.sel.value=12
+    dut.inp12.value=random.randint(0,3)
+    await Timer (2,units='ns')
+    assert dut.out.value== dut.inp12.value,f'mux output is incorrect:{dut.inp12.value}!={dut.out.value}'
+
+@cocotb.test()
+async def test_mux_inp30(dut):
+    'Test for inp30'
+    dut.sel.value=30
+    dut.inp30.value=random.randint(0,3)
+    await Timer (2,units='ns')
+    assert dut.out.value==dut.inp30.value,f'mux output is incorrect:{dut.inp30.value}!={dut.out.value}'
